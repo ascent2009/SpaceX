31a1ab931c3219b39d22506da09e7d412af1e4c1
ascent2009 <ascent2009@hotmail.com>
2020-12-27 20:08:26 +0300

    Updates

---
+++ precache-manifest.85ae29515fdf745f8e48ece2025d51d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19dc0faf4e0da6f45347ff9129f213a",
+    "url": "/space-x/index.html"
+  },
+  {
+    "revision": "8ee10f353a343488d039",
+    "url": "/space-x/static/css/main.21849f00.chunk.css"
+  },
+  {
+    "revision": "e8df018255d8c37e065b",
+    "url": "/space-x/static/js/2.9cc31415.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/space-x/static/js/2.9cc31415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee10f353a343488d039",
+    "url": "/space-x/static/js/main.a3e8bada.chunk.js"
+  },
+  {
+    "revision": "4cd5eada8ae2c6e13aa7",
+    "url": "/space-x/static/js/runtime-main.ed54ab60.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/space-x/static/media/logo.d5ab31ce.svg"
+  }
+]);
